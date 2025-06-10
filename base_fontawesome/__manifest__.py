b54c971fdd4a2a904c46d7e0c99931d40ec63fa3
Augusto Costa <gustotc@gmail.com>
2025-06-10 10:12:51 -0300

    Atualiza fontawesome para vs 6 módulo 'base_fontawesome'

--- base_fontawesome/__manifest__.py
+++ base_fontawesome/__manifest__.py
@@ -1,3 +1,4 @@
+# Copyright 2025 Augusto Costa
 # Copyright 2017 Simone Orsi
 # Copyright 2018 Creu Blanca
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
@@ -5,7 +6,7 @@
 {
     'name': 'Base Fontawesome',
     'summary': """Up to date Fontawesome resources.""",
-    'version': '12.0.5.16.0',
+    'version': '12.0.6.0.0',
     'license': 'LGPL-3',
     'website': 'https://github.com/OCA/server-tools',
     'author': 'Camptocamp,Creu Blanca,Odoo Community Association (OCA)',
